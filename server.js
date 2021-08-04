0b33d22bb0734b45ea05a4bf354bdc7b3e01b0de
jdbooth9516 <jboothwebdev@gmail.com>
2021-08-04 14:30:04 -0700

    connected to the database

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect to the database
+
+connectDB();
+
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server stated on port ${PORT}`));
+
+
