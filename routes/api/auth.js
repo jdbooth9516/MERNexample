b1987e474893a07e7743c8194fe55c7409bcf5b5
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-08-07 20:19:41 -0700

    added the get request of the profiles

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
-
+const config = require('config');
+const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 const User = require("../../models/User")
 
 
@@ -19,6 +22,63 @@ router.get('/', auth, async(req, res) =>{
     }
     }
 );
+
+// Route: POST api/auth
+//DESC: login route
+//ACCESS: auth
+router.post('/', [
+    
+    // this is the validation. 
+    body('email', "Please include a valid email").isEmail(),
+    body('password', "Password is required").exists()
+
+],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) { 
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    // destructer the req.body. 
+    const { email, password} = req.body;
+
+    try  { 
+        // checks to see if the user already exsists
+        let user = await User.findOne({email})
+        if(!user) { 
+             return res.status(400).json({errors: [{msg: 'Invaild Credentials'}]})
+        }
+
+        // verify that the passwords match
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            //use the same message here for security reasons.
+            return res.status(400).json({ errors: [{ msg: 'Invaild Credentials' }] });
+        }
+
+        // this is the information that will get sent with the token
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (error, token) => {
+                if (error) throw error;
+                res.json({ token });
+            }
+        );
+    } catch (error) {
+        console.error(error.message)
+
+    }
+
+   
+});
 
 
 module.exports = router; 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,41 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth')
+const Profile = require("../../models/Profile")
+const { body, validationResult } = require('express-validator');
 
+const User = require('../../models/User')
 
-// Route: GET api/profile
-//DESC: test route
-//ACCESS: Public
-router.get('/', (req, res) => res.send('Profile Route'));
+// Route: GET api/profile/me
+//DESC: Get a single user profile 
+//ACCESS: Private 
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.send(500);
+  }
+});
+
+// Route: POST api/profile/
+//DESC: Create or update user profile 
+//ACCESS: Private 
+router.pst('/', [auth,
+  body('status', "Status is required").not().isEmpty(),
+  body('skills', 'Skills is required').not().isEmptry()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+  
+} )
 
 module.exports = router; 
